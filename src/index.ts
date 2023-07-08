f9f309c1d1a2318346fdd865cc31652c8e13ab2e
John Marlo Lapiz <lazuee.dev@gmail.com>
2023-07-08 08:31:01 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,7 @@ export class Poe {
 	#queue_pending = 0;
 
 	#url = new URL("https://poe.com");
+	#url_home = "/Sage";
 	#url_gql = "/api/gql_POST";
 	#url_settings = "/api/settings";
 
@@ -65,16 +66,16 @@ export class Poe {
 	chatDataKey = "chatOfBotHandle";
 
 	constructor(options: ClientOptions) {
-		this.#logger = options?.logger || {};
-		this.#display_name = options.displayName || "Sage";
+		this.#logger = options?.logger ?? {};
+		this.#display_name = options?.displayName ?? "Sage";
 		this.#request = axios.create({
 			baseURL: this.#url.origin,
 			timeout: 60000,
 			httpAgent: new httpAgent({ keepAlive: true }),
 			httpsAgent: new httpsAgent({ keepAlive: true })
 		});
-		this.#max_retries = options.request?.maxRetries || 20;
-		this.#retry_delay = options.request?.retryDelay || 2000;
+		this.#max_retries = options?.request?.maxRetries ?? 20;
+		this.#retry_delay = options?.request?.retryDelay ?? 2000;
 
 		this.#headers = {
 			"User-Agent": getRandom(),
@@ -168,7 +169,8 @@ export class Poe {
 		this.#device_id = this.#getDeviceID();
 
 		if (!this.#chat_data || !this.#viewer) {
-			throw new Error(`Poe API has changed! Please update the ${!this.#viewer && "viewerKey" || !this.#chat_data && "chatDataKey"}.`)
+			const missingDataKey = !this.#viewer ? "viewerKey" : "chatDataKey";
+			throw new Error(`Poe API has changed! Please update the ${missingDataKey}.`);
 		}
 
 		await this.#subscribe();
@@ -178,11 +180,12 @@ export class Poe {
 	async destroy() {
 		await this.#disconnectWS();
 	}
+
 	async #getNextData() {
 		this.#logger.info?.("Downloading next_data...");
 
 		try {
-			const result = await this.#request.get(this.#url.origin);
+			const result = await this.#request.get(this.#url_home);
 			const jsonRegex = /<script id="__NEXT_DATA__" type="application\/json">(.+?)<\/script>/;
 			const jsonText = jsonRegex.exec(result["data"])![1];
 			const nextData = JSON.parse(jsonText);
@@ -221,9 +224,8 @@ export class Poe {
 	#getDeviceID() {
 		const userId = this.#viewer?.poeUser?.id;
 		const device_id_path = join(dirname(fileURLToPath(import.meta.url)), "..", "poe_device.json");
-		let device_ids: Record<string, string> = {};
+		const device_ids = existsSync(device_id_path) ? JSON.parse(readFileSync(device_id_path, "utf8")) : {};
 
-		if (existsSync(device_id_path)) device_ids = JSON.parse(readFileSync(device_id_path, "utf8"));
 		if (device_ids.hasOwnProperty(userId)) return device_ids[userId];
 
 		const device_id = uuidv4();
@@ -258,20 +260,22 @@ export class Poe {
 	}
 
 	async #send_query(queryName: string, variables: Record<string, any>, queryDisplayName?: string) {
-		for (let i = 0; i < 20; i++) {
-			const payload = { query: queries[queryName], variables };
+		for (let i = 0; i < this.#max_retries; i++) {
+			const payload = { query: queries[queryName], variables};
 			if (queryDisplayName) (payload as any)["queryName"] = queryDisplayName;
 
 			const headers = {
 				"poe-formkey": this.#formkey ?? "",
 				"poe-tchannel": this.#channel_data?.channel ?? "",
 				"poe-tag-id": md5(JSON.stringify(payload) + this.#formkey + "WpuLMiXEKKE98j56k"),
-				...this.#headers
+				...this.#headers,
 			};
+
 			const result = await this.#request.post(this.#url_gql, payload, { headers });
+
 			if (!("data" in result.data)) {
-				this.#logger.warn?.(`'${queryName}' returns no data | Retrying (${i + 1}/20)`);
-				await delay(2000);
+				this.#logger.warn?.(`'${queryName}' returns no data | Retrying (${i + 1}/${this.#max_retries})...`);
+				await delay(this.#retry_delay);
 				continue;
 			}
 
@@ -287,27 +291,35 @@ export class Poe {
 		const ws_domain = `tch${Math.floor(Math.random() * 1e6)}`;
 		const ws_url = `wss://${ws_domain}.tch.${this.#channel_data.baseHost}/up/${this.#channel_data.boxName}/updates${query}`;
 
-		this.#ws_connected = false;
 		this.#ws = new WebSocket(ws_url, {
-			headers: {
-				"User-Agent": this.#headers["User-Agent"]!
-			},
-			rejectUnauthorized: false
-		})
-			.on("message", (message) => {
-				this.#onMessage(message);
-			})
-			.on("open", () => {
-				this.#ws_connected = true;
-			})
-			.on("close", () => {
-				this.#ws_connected = false;
-			})
-			.on("error", (error) => {
-				this.#logger.error?.("Error occured in connectWS", error);
-				this.#disconnectWS();
+			headers: { "User-Agent": this.#headers["User-Agent"]! },
+			rejectUnauthorized: false,
+		});
+
+		await new Promise((resolve, reject) => {
+			this.#ws!.on("open", () => {
+				resolve(true);
 			});
-		while (!this.#ws_connected) await delay(10);
+
+			this.#ws!.on("error", (error) => {
+				reject(error);
+			});
+		});
+
+		this.#ws.on("message", (message) => {
+			this.#onMessage(message);
+		});
+
+		this.#ws.on("close", () => {
+			this.#ws_connected = false;
+		});
+
+		this.#ws.on("error", (error) => {
+			this.#logger.error?.("Error occurred in connectWS", error);
+			this.#disconnectWS();
+		});
+
+		this.#ws_connected = true;
 	}
 
 	#disconnectWS() {
@@ -317,33 +329,28 @@ export class Poe {
 
 	async #onMessage(data: WebSocket.RawData) {
 		try {
-			const { messages } = JSON.parse(data.toString()) ?? {};
-			// If the data doesn't contain any messages, return.
+			const { messages } = JSON.parse(data.toString());
 			if (!Array.isArray(messages)) return;
 
 			for (const message_str of messages) {
 				const message_data = JSON.parse(message_str);
-				if (message_data["message_type"] != "subscriptionUpdate") continue;
+				if (message_data?.message_type !== "subscriptionUpdate") continue;
 
-				const message = message_data["payload"]["data"]["messageAdded"];
+				const message = message_data?.payload?.data?.messageAdded;
 				if (!message) return;
 
-				if ("suggestedReplies" in message && Array.isArray(message["suggestedReplies"])) {
-					this.suggested_replies[message["messageId"]] = [...message["suggestedReplies"]];
-					this.suggested_replies_updated[message["messageId"]] = Date.now();
+				if (message.suggestedReplies && Array.isArray(message.suggestedReplies)) {
+					this.suggested_replies[message.messageId] = [...message.suggestedReplies];
+					this.suggested_replies_updated[message.messageId] = Date.now();
 				}
 
-				const copiedDict = Object.assign({}, this.active_messages);
+				const copiedDict = { ...this.active_messages };
 				for (const [key, value] of Object.entries(copiedDict)) {
-					// Add the message to the queue that matches its message id
-					if (value === message["messageId"] && key in this.message_queues) {
+					if (value === message.messageId && this.message_queues[key]) {
 						this.message_queues[key].push(message);
 						return;
-					}
-
-					// The response id is tied to the human message id
-					else if (key !== "pending" && value === null && message["state"] !== "complete") {
-						this.active_messages[key] = message["messageId"];
+					} else if (key !== "pending" && value === null && message.state !== "complete") {
+						this.active_messages[key] = message.messageId;
 						this.message_queues[key].push(message);
 					}
 				}
@@ -360,10 +367,14 @@ export class Poe {
 			if (this.#ws && this.#ws.readyState === WebSocket.OPEN) this.#ws.ping();
 			this.#ws?.once("pong", () => resolve("ok"));
 		});
-		const timeout = new Promise((resolve) => setTimeout(() => resolve("timeout"), 5000));
+
+		const timeout = new Promise((resolve) => {
+			setTimeout(() => resolve("timeout"), 5000);
+		});
+
 		const result = await Promise.race([pong, timeout]);
 
-		if (result == "ok") return true;
+		if (result === "ok") return true;
 		else {
 			this.#logger.warn?.("Websocket ping timed out.");
 			this.#ws_connected = false;
@@ -379,16 +390,14 @@ export class Poe {
 			await this.#pingWS();
 			await this.#reconnect();
 
-			// Wait until the completion of the active message before sending another one.
 			while (Object.values(this.active_messages).includes(null)) await delay(10);
 
-			// In progress, please wait for further updates of response.
 			this.active_messages["pending"] = null;
 
 			const messageData = await this.#send_query("SendMessageMutation", {
-				bot: this.#chat_data!["defaultBotObject"]["model"],
+				bot: this.#chat_data?.defaultBotObject?.model,
 				query: typeof prompt === "object" ? this.#prompt(prompt) : prompt,
-				chatId: this.#chat_data!["chatId"],
+				chatId: this.#chat_data?.chatId,
 				source: null,
 				clientNonce: generateNonce(16),
 				sdid: this.#device_id,
@@ -396,17 +405,12 @@ export class Poe {
 			});
 
 			delete this.active_messages["pending"];
-			if (!messageData["data"]["messageEdgeCreate"]["message"]) throw new Error(`Daily limit reached for ${this.#display_name}.`);
+			const humanMessage = messageData?.data?.messageEdgeCreate?.message;
+			if (!humanMessage) throw new Error(`Daily limit reached for ${this.#display_name}.`);
 
-			let humanMessageId;
-			try {
-				const humanMessage = messageData["data"]["messageEdgeCreate"]["message"];
-				humanMessageId = humanMessage["node"]["messageId"];
-			} catch (error) {
-				throw new Error(`An unknown error occured. Raw response data: ${messageData}`);
-			}
+			let humanMessageId = humanMessage?.node?.messageId ?? null;
+			if (!humanMessageId) throw new Error(`Something went wrong while sending the message to ${this.#display_name}.`);
 
-			// The current message is awaiting for response
 			this.active_messages[humanMessageId] = null;
 			this.message_queues[humanMessageId] = [];
 
@@ -425,13 +429,12 @@ export class Poe {
 						continue;
 					}
 
-					// Wait until the message is marked as complete before proceeding.
 					if (_message["state"] === "complete") {
 						if (lastText && _message["messageId"] === messageId) break;
 						else continue;
 					}
 
-					// Update response
+
 					_message["text_new"] = _message["text"].substring(lastText.length);
 					_message["suggestedReplies"] = this.suggested_replies[_message["messageId"]] ?? [];
 					_message["suggestedRepliesUpdated"] = this.suggested_replies_updated[_message["messageId"]] ?? null;
@@ -464,13 +467,12 @@ export class Poe {
 	}
 
 	async break_message() {
-		const result = await this.#send_query("AddMessageBreakMutation", {
-			chatId: this.#chat_data!["chatId"]
+		await this.#send_query("AddMessageBreakMutation", {
+			chatId: this.#chat_data?.chatId
 		});
-		return result["data"]["messageBreakCreate"]["message"];
 	}
 
-	async history(count = 25, cursor = null) {
+	async history(count = 25, cursor = null): Promise<Record<string, any>[]> {
 		try {
 			const result = await this.#send_query("ChatListPaginationQuery", {
 				count: count,
@@ -478,66 +480,51 @@ export class Poe {
 				id: this.#chat_data!["id"]
 			});
 
-			const messages = result["data"]["node"]["messagesConnection"]["edges"];
-			if (!messages) throw new Error("No messages found in result");
+			const messages = result?.data?.node?.messagesConnection?.edges ?? [];
 
 			return messages;
 		} catch (error) {
-			return null;
+			return [];
 		}
 	}
 
 	async delete(...message_ids: (number | number[])[]) {
 		try {
-			// Flatten the array of arrays and ensure each item is an integer
 			const ids = message_ids
 				.flat()
-				.map((id) => parseInt(String(id)))
+				.map((id) => parseInt(String(id), 10))
 				.filter((id) => !isNaN(id));
 
-			// If no valid message IDs are provided, return null
-			if (ids.length === 0) return null;
+			if (ids.length === 0) return;
 
-			const result = await this.#send_query("DeleteMessageMutation", {
-				messageIds: ids
-			});
+			await this.#send_query("DeleteMessageMutation", { messageIds: ids });
 
-			return result;
+			return;
 		} catch (error) {
-			return null;
+			return;
 		}
 	}
 
 	async purge(count = -1) {
 		try {
-			// Set up a loop to delete messages in batches of 50
 			let last_messages = (await this.history(50)).reverse();
 			while (last_messages.length) {
 				const message_ids = [];
 
-				// Build an array of message IDs to delete
 				for (const message of last_messages) {
 					if (count === 0) break;
 					count--;
 
-					const message_id = message["node"]["messageId"];
+					const message_id = message?.node?.messageId;
 					if (message_id) message_ids.push(parseInt(message_id));
 				}
 
-				// Delete the messages
-				await this.delete(message_ids);
+				if (message_ids.length > 0) await this.delete(message_ids);
+				if (count === 0) return;
 
-				// If we've reached the message count limit, stop deleting messages
-				if (count === 0) {
-					// console.info(`Deleted ${message_ids.length} messages`);
-					return;
-				}
-
-				// Get the next batch of messages to delete
 				last_messages = (await this.history(50)).reverse();
 			}
 
-			// console.info('No more messages left to delete.');
 			return;
 		} catch (error) {
 			return;
